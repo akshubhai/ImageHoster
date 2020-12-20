c98d5a566b213a11ed6eaffe2290f0115c07c8ea
akshubhai <akshubhai.reloaded@gmail.com>
2020-12-21 00:40:21 +0530

    Create new class Comment.java

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,66 @@
+package ImageHoster.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+
+@Table(name = "comments")
+public class Comment {
+
+    //@Id annotation specifies that the corresponding attribute is a primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column annotation specifies that the attribute will be mapped to the column in the database.
+    //Here the column name is explicitly mentioned as 'id'
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "text", length = 256)
+    private String text;
+
+    @Column(name = "createdDate")
+    private LocalDate createdDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user")
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
