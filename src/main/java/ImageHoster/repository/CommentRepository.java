6eed4e0f6e821a59c85c91023ef2b8304ed7da8f
akshubhai <akshubhai.reloaded@gmail.com>
2020-12-21 12:39:33 +0530

    Created addNewComment func in repo class

--- src/main/java/ImageHoster/repository/CommentRepository.java
+++ src/main/java/ImageHoster/repository/CommentRepository.java
@@ -1,4 +1,29 @@
 package ImageHoster.repository;
 
+import ImageHoster.model.Comment;
+import ImageHoster.model.Image;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+
+@Repository
 public class CommentRepository {
+
+    @PersistenceUnit(unitName = "imageHoster")
+    private EntityManagerFactory emf;
+
+    public Comment addNewComment(Comment newComment){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            em.persist(newComment);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+        return newComment;
+    }
+
 }
