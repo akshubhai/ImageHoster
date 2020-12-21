161d77b84d6c53474473676133b5bba993c76aef
akshubhai <akshubhai.reloaded@gmail.com>
2020-12-21 12:41:44 +0530

    Create addNewCOmment func in service class

--- src/main/java/ImageHoster/service/CommentService.java
+++ src/main/java/ImageHoster/service/CommentService.java
@@ -1,10 +1,16 @@
 package ImageHoster.service;
 
+import ImageHoster.model.Comment;
+import ImageHoster.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CommentService {
 
     @Autowired
     private CommentRepository commentRepository;
+
+    public Comment addNewComment(Comment newComment){
+        commentRepository.addNewComment(newComment);
+    }
 
 }
